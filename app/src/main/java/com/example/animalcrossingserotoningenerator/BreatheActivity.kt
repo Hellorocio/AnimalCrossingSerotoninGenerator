1288135ed09b9536620c07a96b1d713573bfa408
Amber Smith <amber.smith@utexas.edu>
2019-12-02 08:46:07 -0600

    Attempted to add isabelle animation (not tested)

--- app/src/main/java/com/example/animalcrossingserotoningenerator/BreatheActivity.kt
+++ app/src/main/java/com/example/animalcrossingserotoningenerator/BreatheActivity.kt
@@ -1,5 +1,6 @@
 package com.example.animalcrossingserotoningenerator
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
@@ -12,11 +13,21 @@ import java.util.*
 
 class BreatheActivity : AppCompatActivity() {
 
+    private val breatheInTime : Long = 2000
+    private val holdTime : Long = 3500
+    private val breatheOutTime : Long = 4000
+
+    private lateinit var isabelleAnim : AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.breathe_main)
-        countDown()
 
+        isabelleBreatheIV.apply {
+            setBackgroundResource(R.drawable.isabelle_anim)
+            isabelleAnim = background as AnimationDrawable
+        }
+        countDown()
     }
 
     // Count down at the start to prepare for breathing exercise
@@ -33,6 +44,7 @@ class BreatheActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                isabelleAnim.start()
                 breatheIn()
             }
         }.start()
@@ -42,7 +54,7 @@ class BreatheActivity : AppCompatActivity() {
     fun breatheIn() {
         breatheTV.text = "Breathe in through your nose"
 
-        var breatheTimer = object : CountDownTimer(4000, 10) {
+        var breatheTimer = object : CountDownTimer(breatheInTime, 10) {
 
             override fun onTick(millisUntilFinished: Long) {
 
@@ -63,7 +75,7 @@ class BreatheActivity : AppCompatActivity() {
     fun holdBreath() {
         breatheTV.text = "Hold"
 
-        var breatheTimer = object : CountDownTimer(7000, 1000) {
+        var breatheTimer = object : CountDownTimer(holdTime, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
             }
@@ -77,7 +89,7 @@ class BreatheActivity : AppCompatActivity() {
     fun breatheOut() {
         breatheTV.text = "Breathe out through your mouth"
 
-        var breatheTimer = object : CountDownTimer(8000, 10) {
+        var breatheTimer = object : CountDownTimer(breatheOutTime, 10) {
 
             override fun onTick(millisUntilFinished: Long) {
 
